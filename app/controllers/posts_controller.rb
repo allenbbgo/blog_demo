8446801863a04440bc5a449cd0d8c98b6dc5b714
allenbbgo <c8c886@gmail.com>
2019-01-30 17:21:23 +0800

    devise install ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+    before_action :authenticate_user!, except: [:index, :show]
     def index
 
         @post = Post.all.order('created_at DESC')
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 
 
     def new 
-        @post = Post.new
+        @post=Post.new
 
 
     end
@@ -16,47 +16,45 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new(post_params)
+
         if @post.save
             redirect_to @post
-          else
+        else
             render 'new'
-          end
-
+        end
     end
 
 
     def show
         @post=Post.find(params[:id])
 
-
-
     end
-
 
 
     def edit
         @post = Post.find(params[:id])
-      end
-      
-      def update
+    end
+
+    def update 
         @post = Post.find(params[:id])
-      
-        if @post.update(params[:post].permit(:title, :body))
-          redirect_to @post
+
+        if @post.update(params[:post].permit(:title,:body))
+            redirect_to @post
         else
-          render 'edit'
+            render 'edit'
         end
-      end
 
-      def destroy
-        @post = Post.find(params[:id])
-          @post.destroy
-       
-          redirect_to root_path
-      end
+    end
 
 
+    def destroy
+        @post =Post.find(params[:id])
 
+        @post.destroy
+
+        redirect_to root_path
+
+    end
 
     private
 
