cdbd6125a5ffe03a0ecbe3b7c55bc9a227ef68d8
allenbbgo <c8c886@gmail.com>
2019-01-30 16:25:27 +0800

    edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,10 +34,26 @@ class PostsController < ApplicationController
 
 
 
+    def edit
+        @post = Post.find(params[:id])
+      end
+      
+      def update
+        @post = Post.find(params[:id])
+      
+        if @post.update(params[:post].permit(:title, :body))
+          redirect_to @post
+        else
+          render 'edit'
+        end
+      end
 
-
-
-
+      def destroy
+        @post = Post.find(params[:id])
+          @post.destroy
+       
+          redirect_to root_path
+      end
 
 
 
